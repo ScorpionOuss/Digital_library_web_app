f94a1d4678fac01c393afc23a1e57297f5868b06
Halima Mounsit <halima.mounsit@grenoble-inp.org>
2021-04-11 13:48:08 -0400

    maybe for history

---
+++ src/main/java/servlets/modeLecture/AlterHisAndRead.java
@@ -0,0 +1,51 @@
+package servlets.modeLecture;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.Choix;
+import beans.Historique;
+
+/**
+ * Servlet implementation class AlterHisAndRead
+ */
+@WebServlet("/AlterHisAndRead")
+public class AlterHisAndRead extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public static final String HISTORY = "historique";
+	public static final String Choice = "idChoice";
+	public static final String secServlet = "/LireParagraph";
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AlterHisAndRead() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/* We will just alter the history and redirect toward the servlet that will show the paragraph */
+		Historique his = (Historique) request.getSession().getAttribute(HISTORY);
+		/* Not the perfect choice */
+		int breakAt = Integer.parseInt(request.getParameter(Choice));
+		LinkedList<Choix> newHis = new LinkedList<Choix>();
+		for (Choix ch : his.getHisChoices()) {
+			if (ch.getIdChoice() == breakAt) {
+				break;
+			}
+			newHis.add(ch);
+		}
+		((Historique) request.getSession().getAttribute(HISTORY)).setHisChoices(newHis);
+		/* Redirect to the paragraph reading Servlet */
+		this.getServletContext().getRequestDispatcher( secServlet).forward( request, response );
+	}
+}
