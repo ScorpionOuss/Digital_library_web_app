0c6a391afa7ec4e82e7ddc8132e1f2dd483c9e30
Oussama KADDAMI <oussama.kaddami @grenoble-inp.org>
2021-04-13 13:28:29 +0200

    edition

--- src/main/java/servlets/WriteParagraph.java
+++ src/main/java/servlets/WriteParagraph.java
@@ -10,10 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
-import beans.Paragraphe;
-import dao.ChoixDAO;
-import dao.ParagrapheDAO;
-
 /**
  * Servlet implementation class LireParagraph
  */
